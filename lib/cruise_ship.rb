7a47a62d8dbf80dd5f673b6bb8aec9bfd56f058c
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-01 18:24:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,9 @@
 
 def select_winner(passengers)
   winner =""
-  passengers.each { |suite, name| if suite == :suite_a && name.start_with?("A") winner = "name" }
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = "name"
+    end
   winner
 end
